4361239c368b145c73ab738f6929a2361b604044
Roberto Kedmenec <kedmenecr@Robertos-MacBook-Pro-2.local>
2018-06-28 10:37:54 +0200

    Finished server with get,post and delete the item.

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items');
+
+
+
 
 const app = express();
 
@@ -20,7 +24,11 @@ mongoose
 	.then(() => console.log("MongoDB Connected"))
 	.catch(err => console.log('err :', err));
 
-const port = process.env.PORT || 5000;
+// use routers
+
+app.use('/api/items', items);
+
+const port = process.env.PORT || 9000;
 
 app.listen(port, () => console.log(`Servert started on port ${port}`));
 
